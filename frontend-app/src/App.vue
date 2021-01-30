6e9a9eb7015397587f0ca0bd3340431ee8dc2557
lumosindigo <77829218+lumosindigo@users.noreply.github.com>
2021-01-30 11:10:16 +0200

    Implement simple welcome screen (#2)
    * Start to investigate BoostrapVue. WIP
    
    * More changes
    
    * Add http support via vue-resource
    
    Co-authored-by: ryan-vandermerwe <ryanvandermerwe51@gmail.com>

--- frontend-app/src/App.vue
+++ frontend-app/src/App.vue
@@ -1,45 +1,106 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
-      </li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li>
-        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
-      </li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-    <simple-button></simple-button>
+    <b-form @submit="onSubmit" @reset="onReset">
+      <br />
+      <br />
+      <br />
+      <b-form-group id="input-group-1" label-for="input-1">
+        <b-row>
+          <b-col sm="4">
+            <label class="label_user_pass">Username</label>
+          </b-col>
+          <b-col sm="5">
+            <b-form-input
+              id="input-1"
+              size="sm"
+              v-model="loginForm.username"
+              type="text"
+              placeholder="Enter username"
+              required
+            ></b-form-input>
+          </b-col>
+        </b-row>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label-for="input-2">
+        <b-row>
+          <b-col sm="4">
+            <label class="label_user_pass">Password</label>
+          </b-col>
+          <b-col sm="5">
+            <b-form-input
+              type="password"
+              size="sm"
+              id="text-password"
+              v-model="loginForm.password"
+              aria-describedby="password-help-block"
+            ></b-form-input>
+            <b-form-text id="password-help-block">
+              Your password must be 8-20 characters long, contain letters and
+              numbers, and must not contain spaces, special characters, or
+              emoji.
+            </b-form-text>
+          </b-col>
+        </b-row>
+      </b-form-group>
+      <br />
+      <b-row>
+        <b-col sm="4"></b-col>
+        <b-col sm="1">
+          <b-button type="submit" pill variant="outline-primary" size="sm">Login</b-button>
+          <b-button type="reset" pill variant="outline-danger" size="sm">Reset</b-button>
+        </b-col>
+        <b-col sm="1">
+          <b-link class="register-link" href="#foo">Click to register</b-link>
+        </b-col>
+      </b-row>
+
+      <!-- <b-button type="submit" variant="primary" size="sm">Login</b-button>
+      <b-button type="reset" variant="danger" size="sm">Reset</b-button> -->
+    </b-form>
+    <!-- <simple-button></simple-button> -->
   </div>
 </template>
 
 <script>
-import SimpleButton from './SimpleButton.vue';
+import SimpleButton from "./SimpleButton.vue";
+import { BForm } from "bootstrap-vue";
 
 export default {
   name: "app",
   components: {
-    'simple-button': SimpleButton
+    "simple-button": SimpleButton,
+    "b-form": BForm,
   },
   data() {
     return {
-      msg: "Welcome to Your Vue.js App",
+      msg: "Welcome to SkoolBoekie",
+      loginForm: {
+        username: "",
+        password: "",
+      },
     };
+  },
+
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      this.$http.post('http://localhost:8080/user', this.loginForm)
+      .then(response => {
+          console.log(response);
+      }, error => {
+        console.log(error);
+      });
+
+      alert(JSON.stringify(this.loginForm));
+    },
+    onReset(event) {
+      event.preventDefault();
+      // Reset our form values
+      this.loginForm.username = "";
+      this.loginForm.password = "";
+    },
   },
 };
 </script>
@@ -51,7 +112,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 250px;
 }
 
 h1,
@@ -69,7 +130,17 @@ li {
   margin: 0 10px;
 }
 
-a {
-  color: #42b983;
+// a {
+//   color: #42b983;
+// }
+
+.label_user_pass {
+  padding-left: 520px;
+  font-size: 17;
 }
+
+.register-link {
+  font-size: 13px;
+}
+
 </style>
--- frontend-app/src/main.js
+++ frontend-app/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import VueResource from 'vue-resource';
 import App from './App.vue';
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -7,6 +8,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(VueResource);
 
 new Vue({
   el: '#app',
